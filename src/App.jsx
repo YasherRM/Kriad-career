07232d1b0b4eef81ec74efd46d22991fb45a1fee
Prasanna-2527 <prasannaone27@gmail.com>
2024-10-24 19:00:53 +0530

    AboutPage commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 
 import HomePage from "./Components/HomePage";
 import SignUpPage from "./Components/SignUpPage";
+import AboutPage from "./Components/AboutPage";
 
 function App() {
   return (
     <>
       <HomePage />
       <SignUpPage />
+      <AboutPage />
     </>
   );
 }
